3464acfd41fbe29bf8f6251022b9caac12f95d18
Archo21 <87740574+Archo21@users.noreply.github.com>
2021-11-19 09:18:47 -0800

    updated

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  // find all tags
+  Tag.findAll({
+    // be sure to include its associated Category and Tag data
+    include:[
+    {
+      model:Product,
+      through:ProductTag
+    }]
+  })
+  .then((tags)=>{
+    res.status(200).json(tags);
+  })
+  .catch((error)=>{
+    res.status(500).json(error);
+  })
+  
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  Tag.findOne({
+    where:{
+      id:req.params.id,
+    },
+    // be sure to include its associated Category and Tag data
+    include:[
+    {
+      model:Product,
+      through:ProductTag
+    }]
+  })
+  .then((tag)=>{
+ res.json(tag);
+  })
+  .catch((error)=>{
+    res.status(400).json(error);
+  })
+  
+});
+
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create(
+    req.body 
+   )
+   .then((tag)=>{
+  res.status(200).json(tag);
+   })
+   .catch((error)=>{
+     res.status(400).json(error);
+   })
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+  Tag.update(req.body,{
+    where:{
+      id:req.params.id
+    },
+   
+  })
+  .then((tag)=>{
+ res.status(200).json(tag);
+  })
+  .catch((error)=>{
+    res.status(400).json(error);
+  })
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id:req.params.id
+    },
+   
+  })
+  .then((tag)=>{
+ res.status(200).json(tag);
+  })
+  .catch((error)=>{
+    res.status(400).json(error);
+  })
+});
+
+module.exports = router;
